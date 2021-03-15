d36a2067b9c74ee9d688f9f540d9af63efce3a75
shokun1209 <shoya@satoushouyanoMacBook-Air.local>
2021-03-15 17:51:40 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -10,6 +11,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
       render :index
     end
   end
@@ -20,3 +22,9 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
+
+
+# prefixのtweet_pathを使う際は、:idに何を入れるか指定しなければいけない
+# tweet_path(3) => /tweets/3
+# tweet_path("hoge") => /tweets/hoge
+# tweet_path(@tweet.id) => /tweets/@tweetのid
